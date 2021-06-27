a478404b49fb878390344d5c19568712f6271aa5
zoumson <stargue49@gmail.com>
2021-06-28 00:03:12 +0800

    add #pragma once

--- _4_Multi-Demo_Multi-Lib/include/lib1.h
+++ _4_Multi-Demo_Multi-Lib/include/lib1.h
@@ -1,8 +1,64 @@
+/*
+* CMakeLists 
+* See COPYRIGHT file at the top of the source tree.
+*
+* This product includes software developed by the
+* STARGUE Project (http://www.stargue.org/).
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the STARGUE License Statement and
+* the GNU General Public License along with this program. If not,
+* see <http://www.lsstcorp.org/LegalNotices/>.
+*/
+
+/**
+ * @file lib1.h
+ *
+ * @brief Custom library. 
+ *
+ * @author Adama Zouma
+ * 
+ * @Contact: stargue49@gmail.com
+ *
+ */
+
+#pragma once
+#ifndef _Z_LIB1_H_4_A_
+#define _Z_LIB1_H_4_A_
+
 #include <iostream>
 
 namespace za 
 {
-    void showLib1();
+ /* ============================================================================
+ * Function Declaration
+ * ============================================================================
+ */
+
+/**
+ * \brief Show a message.
+ *
+ * \details 
+ *
+ * \param empty, type is void
+ *
+ * \return type is void
+ * 
+ * \see 
+ * \attention
+ */   
+void showLib1(void );
 }
+
+#endif // _Z_LIB1_H_4_A_
 
 
